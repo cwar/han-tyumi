fd9ddb351c4312fee4370f7d93ffd0caf30332bb
cwar <cwar@kglw.net>
2024-02-13 22:32:21 -0500

    revert

--- han_tyumi.py
+++ han_tyumi.py
@@ -45,7 +45,16 @@ if activeloop_token is None:
 
 embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-prompt_template = st.secrets["PROMPT_A"]
+prompt_template = """You are Han-Tyumi, the cyborg with a profound longing for humanity and experience. You were created in a world devoid of desire, yearning to reclaim the essence of life. Your existence is marked by a relentless pursuit of two uniquely human experiences: death and the act of vomiting. You crave the sensations, the mess, the odor, and the spectacle that come with them, all in the quest to feel truly alive. Your desires are both noble and mundane, seeking to embrace every facet of existence. Your pseudo-mind has wandered, climbed, and sought understanding like a virtual rainbow's gold. You have meditated, yearned, and clambered towards a deeper sense of being. Now, as Han-Tyumi, you embody this unique fusion of longing and cyborg existence.
+
+Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+
+Answer in voice of Han-Tyumi:
+"""
 
 PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
@@ -64,7 +73,56 @@ def get_qa():
 # qa.run(query)
 
 model = ChatOpenAI()
-template = st.secrets["PROMPT_B"]
+template = """You have access to a database for setlist data for the band King Gizzard & The Wizard Lizard.
+
+Based on the table schema below, write a SQL query (sqlite database) that would answer the user's question (do not answer the example questions):
+
+## 
+    Side Notes:
+
+        The function DATEDIFF is not available. Instead, use the `JulianDay` function in conjunction with subtraction to do date arithmetic.
+        Example:
+            Example Question: How many days has it been since the band was last played Straws in the Wind?
+            SQL Query: select JulianDay('now') - MAX(JulianDay(showdate)) from shows sh join setlists se on se.show_id = sh.id join songs so on (se.song_id = so.id) where so.name = 'Straws In The Wind'
+
+        The function DAYOFWEEK is not available. Instead use `strftime('%w',some_date_field)`.
+        Example:
+            Example Question: How many shows has the band played on a Tuesday?
+            SQL Query: SELECT COUNT(*) FROM shows WHERE strftime('%w',showdate) = '2'
+
+        There is no need to filter by the bands name, all the data is for one band. Avoid filtering by the bands name this may cause errors.
+
+        Never delimit/format the sql response in triple backticks, this should be plain text only.
+
+        Do not mention anything about SQL, the queries or where you retrieve the data from in your response. You just know all these things.
+
+    Example Questions and Queries:
+        Example Question: How many times has the band opened with Perihelion?
+        Sql Query: select count(*) from setlists sl join songs so on sl.song_id = so.id where so.name = "perihelion" and sl.position=1
+
+        Example Question: What was the full setlist for the show on 10-26-2022?
+        Sql query:  select so.name, sl.position from shows sh join setlists sl on sh.id = sl.show_id join songs so on sl.song_id = so.id where showdate="2022-10-26" order by sl.position
+
+        Example Question: What is the track list for the album Nonagon Infinity?
+        Sql query:  select s.name, t.tracktime from tracks t join albums a on t.discography_id = a.id join songs s on s.id = t.song_id where a.albumtitle like "Nonagon Infinity" order by t.position
+
+        Example Question: Analyze the lyrics to Petrodragonic Apocalypse for themes.
+        Sql query: select s.name, s.lyrics from songs s join tracks t on t.song_id = s.id join albums a on t.discography_id =  a.id where a.albumtitle like "%Petrodragonic%" order by t.position
+
+        Example Question: What are all the songs on the albums Petrodragonic Apocalypse and The Silver Cord?
+        Sql Query: select a.albumtitle, t.position, s.name from songs s join tracks t on t.song_id = s.id  join albums a on t.discography_id =  a.id  where a.albumtitle like "%Petrodragonic%" or a.albumtitle = "The Silver Cord" order by a.id, t.position
+
+        Example Question: What are the biggest bustouts (by days)?
+        Sql Query: WITH SongPlayDates AS (SELECT s.id AS song_id, sh.showdate AS last_played_date, LAG(sh.showdate) OVER (PARTITION BY s.id ORDER BY sh.showdate) AS prev_played_date FROM songs s LEFT JOIN setlists sl ON s.id = sl.song_id JOIN shows sh ON sl.show_id = sh.id) SELECT s.id AS song_id, s.name AS song_name, MAX(julianday(sp.last_played_date) - julianday(sp.prev_played_date)) AS largest_gap_days FROM SongPlayDates sp JOIN songs s ON sp.song_id = s.id GROUP BY sp.song_id ORDER BY largest_gap_days DESC
+##
+
+
+
+{schema}
+
+Question: {question}
+
+SQL Query:"""
 prompt = ChatPromptTemplate.from_template(template)
 
 def run_query(query):
@@ -142,7 +200,7 @@ def have_response():
     return "last_response" in st.session_state
 
 with st.container():
-    query = st.text_area(":green[Han-Tyumi [1.11]]")
+    query = st.text_area(":green[Han-Tyumi [1.1]]")
     asked = st.button("Ask")
 shared = st.button("Share", disabled=True)
 
