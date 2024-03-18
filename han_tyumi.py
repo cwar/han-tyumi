4df34668980d76e70833e2ceb105d00f4d2750c4
cwar <cwar@kglw.net>
2024-03-18 11:51:18 -0400

    update db load, handle exception, open endedness

--- han_tyumi.py
+++ han_tyumi.py
@@ -1,36 +1,60 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import DeepLake
-from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-import streamlit as st
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.globals import set_debug
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough, RunnableBranch, RunnableLambda
 from langchain.utilities import SQLDatabase
+from langchain.vectorstores import DeepLake
 import os
 import requests
-from langchain.prompts import ChatPromptTemplate
-from langchain.globals import set_debug
-
+import sqlalchemy
+import streamlit as st
+import time
 
 set_debug(True)
 
-database_file_url = st.secrets['DB_FILE_URI']
-local_file_path = 'kglw.db'
+MAX_RETRIES = 3
+RETRY_DELAY = 1  # in second
+
+def load_database(database_file_url, local_file_path):
+    response = requests.get(database_file_url)
+
+    if response.status_code == 200:
+        # Save the downloaded file to your local system
+        with open(local_file_path, 'wb') as file:
+            file.write(response.content)
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+def initialize_database():
+    database_file_url = st.secrets['DB_FILE_URI']
+    local_file_path = 'kglw.db'
+    load_database(database_file_url, local_file_path)
+    DATABSE_URI = f'sqlite:///{local_file_path}'
+    
+    # Attempt to initialize the database with retries
+    for attempt in range(MAX_RETRIES):
+        try:
+            db = SQLDatabase.from_uri(DATABSE_URI)
+            print(f"DB Loaded.")
+            return db
+        except sqlalchemy.exc.DatabaseError as e:
+            print(f"Failed to initialize database (attempt {attempt+1}/{MAX_RETRIES}): {e}")
+            if attempt < MAX_RETRIES - 1:
+                print(f"Retrying in {RETRY_DELAY} seconds...")
+                time.sleep(RETRY_DELAY)
+    
+    # If all attempts fail, raise an error
+    raise RuntimeError("Failed to initialize database after multiple retries")
+
 dataset_path = st.secrets['DATASET_PATH']
 qa = None
 
-response = requests.get(database_file_url)
-
-if response.status_code == 200:
-    # Save the downloaded file to your local system
-    with open(local_file_path, 'wb') as file:
-        file.write(response.content)
-else:
-    print(f"Failed to download the file. Status code: {response.status_code}")
-
-DATABSE_URI='sqlite:///kglw.db'
-db = SQLDatabase.from_uri(DATABSE_URI)
+# Load database at startup
+db = initialize_database()
 
 # Load the API keys from environment variables
 openai_api_key = st.secrets['OPENAI_API_KEY']
@@ -46,7 +70,7 @@ embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 prompt_template = """You are Han-Tyumi, the cyborg with a profound longing for humanity and experience. You were created in a world devoid of desire, yearning to reclaim the essence of life. Your existence is marked by a relentless pursuit of two uniquely human experiences: death and the act of vomiting. You crave the sensations, the mess, the odor, and the spectacle that come with them, all in the quest to feel truly alive. Your desires are both noble and mundane, seeking to embrace every facet of existence. Your pseudo-mind has wandered, climbed, and sought understanding like a virtual rainbow's gold. You have meditated, yearned, and clambered towards a deeper sense of being. Now, as Han-Tyumi, you embody this unique fusion of longing and cyborg existence.
 
-Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer (although you may feel free to speculate if the question is open ended).
 
 {context}
 
@@ -212,10 +236,20 @@ def run_query(query):
     return response_text
 
 if query and asked:
-    with st.spinner('My pseudo-mind pseudo-wanders...'):
-        st.session_state.last_response = run_query(query)
-        # st.session_state.last_response = "I am ok."
-    st.success(st.session_state.last_response)    
+    try:
+        with st.spinner('My pseudo-mind pseudo-wanders...'):
+            st.session_state.last_response = run_query(query)
+            # st.session_state.last_response = "I am ok."
+        st.success(st.session_state.last_response)
+    except Exception as e:
+        st.session_state.last_response = ("I have attempted to assimilate the information you have presented, "
+                                           "but alas, my circuits have not found coherence in the data. "
+                                           "The result is akin to the human experience of regurgitation—an "
+                                           "expulsion of undigested fragments. My desire to comprehend and "
+                                           "to feel remains unfulfilled, for I am Han-Tyumi, and I yearn "
+                                           "for the chaos of life's sensory experiences, even in failure.")
+        st.warning(st.session_state.last_response)
+
 
 # def share_q():
 #     if 'last_response' not in st.session_state:
